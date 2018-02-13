dc3724cc62fa73b747b394d09fd11b9a57d0f4c6
Travis Meamber <tmeamber623@gmail.com>
2018-02-13 09:23:23 -0800

    installed the dependencies I'll need

--- collectors_choice/src/App.js
+++ collectors_choice/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Products from './Components/Products/Products';
+import About from './Components/About/About';
+import Home from './Components/Home/Home';
+import Contact from './Components/Contact/Contact';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="navbar">
+        <Link to='/'>
+        <div className='Hm'>Home</div>
+        </Link>
+        <Link to='/Products'>
+        <div className='Prod'>Products</div>
+        </Link>
+        <Link to='/About'>
+        <div className='About'>About Us</div>
+        </Link>
+        <Link to='/Contact'>
+        <div className='Contact'>Contact Us</div>
+        </Link>
+        </div>
+        <div className='routes'>
+        <Route exact path='/' component={Home}/>
+        <Route path='/Products' component={Products}/>
+        <Route path='/About' component={About}/>
+        <Route path='/Contact' component={Contact}/>
+        </div>
       </div>
     );
   }
---
+++ collectors_choice/src/Components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Contact.css';
+
+
+export default class Contact_us extends Component{
+    render () {
+        return(
+            <div>
+                <div className='info'>
+               <p>Contact us at (530) 222-8750</p>
+               <p>Come see us at our location at 1714 Churn Creek Rd, Redding, CA 96002</p>
+               <p>Our hours are 10 AM to 6 PM Monday - Saturday and closed Sundays and Major Holidays</p>
+               </div>
+            </div>
+        )
+    }
+}
