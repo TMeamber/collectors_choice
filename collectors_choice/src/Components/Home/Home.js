8c5d9c12c5e832bac37cebda35c310b7c11c5f9e
Travis Meamber <tmeamber623@gmail.com>
2018-02-14 09:01:10 -0800

    Home page is done

--- collectors_choice/src/Components/Home/Home.js
+++ collectors_choice/src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './Home.css';
-
+import Collectors from '../../Assets/choice_wo.jpg'
 
 export default class Home extends Component{
     render () {
         return(
             <div>
-          
+          <div className="CC_Logo">
+          <img className="Choice_wo" alt="" src={Collectors} />
+          </div>
             </div>
         )
     }
