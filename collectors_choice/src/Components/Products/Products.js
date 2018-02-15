0d90f8846dd21d51b98f7684cc39b3199137d422
Travis Meamber <tmeamber623@gmail.com>
2018-02-15 11:43:41 -0800

    brought in carosuel

--- collectors_choice/src/Components/Products/Products.js
+++ collectors_choice/src/Components/Products/Products.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './Products.css';
-
+import Carousel from 'nuka-carousel';
 
 export default class Products extends Component{
     render () {
         return(
             <div>
                  <div className='products'>
+                 <Carosuel>
                 <img className='' alt='' src={}/> 
                 <img className='' alt='' src={}/>
                 <img className='' alt='' src={}/>  
                 <img className='' alt='' src={}/> 
+                </Carosuel>
                 </div>
             </div>
         )
